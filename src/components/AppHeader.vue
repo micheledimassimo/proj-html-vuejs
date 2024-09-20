8ff4427d34d2d22d83425c9ea5f9171bc85de563
micheledimassimo <mcldms@outlook.it>
2024-09-20 12:37:50 +0200

    v1.0 creato componente header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -2,30 +2,137 @@
 export default {
   data() {
     return { 
-      message: 'Template Vite + Vue'
+      navLinks: [
+        {
+            label: 'Home',
+            url: '#',
+            active: true
+        },
+        {
+            label: 'About Us',
+            url: '#',
+            active: false
+        },
+        {
+            label: 'Facilities',
+            url: '#',
+            active: false
+        },
+        {
+            label: 'Membership',
+            url: '#',
+            active: false
+        },
+        {
+            label: 'Testimonials',
+            url: '#',
+            active: false
+        },
+        {
+            label: 'Blog',
+            url: '#',
+            active: false
+        }
+      ],
+      logoImg: '/images/gym_logo_1x.png'
     }
   }
 }
 </script>
 
 <template>
-  <header>
-    <h1>
-      {{ message }}
-    </h1>
-  </header>
+    <header>
+        <div class="my-row justify-space-between">
+            <img :src="logoImg" alt="Avada Gym">
+            <nav>
+                <ul class="my-row">
+                    <li v-for="(link, i) in navLinks" :key="i">
+                        <a
+                        :class="{
+                            'active': link.active
+                        }" 
+                        :href="link.url">
+                            {{ link.label }}
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+        <div>
+            <div class="my-container">
+                <h2>
+                    no pain no gain
+                </h2>
+                <p>
+                    Motivation is what gets you started. Habit is what keeps you going
+                </p>
+                <button class="my-btn">
+                    <a href="#">
+                        Join us today
+                    </a>
+                </button>
+            </div>
+        </div>
+    </header>
 </template>
 
 <style lang="scss" scoped>
 @use '../assets/scss/partials/variables' as *;
+@use '../assets/scss/main' as *;
 
 header {
-  background-color: $mainBgColor;
+  background-image: url('/images/home-banner.jpg');
+  background-position: 50% 9%;
+  padding: 20px 70px;
   text-align: center;
-  padding: 20px 0;
+  >.my-row {
+    align-items: center;
+    > nav {
+        > ul {
+            list-style: none;
+            li > a {
+                text-decoration: none;
+                text-transform: uppercase;
+                color: white;
+                opacity: 0.8;
+                padding: 10px;
+
+                &:hover,
+                &.active {
+                    color: $mainColor;
+                } 
+            
+            }
+        }
+    }
+  }
+  div >.my-container {
+    margin: 100px 0 180px;
+    h2 {
+        font-size: 7rem;
+        text-transform: uppercase;
+        font-weight: bold;
+        color: white;
+        padding: 100px 0 0;
+    }
+    p {
+        color: $colorLightgray;
+        font-size: 1.5rem;
+        padding: 10px 0 20px;
+    }
+    
+  }
 }
 
-h1 {
-  color: $mainColor;
+.my-btn {
+    cursor: pointer;
+    padding: 10px 20px;
+    border: 1px solid $colorGray;
+    background-color: transparent;
+    > a {
+        text-decoration: none;
+        color: $colorGray;
+    }
 }
+
 </style>
